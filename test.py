ceedb975a0f2e598123cfa84eb8cb81a9d1f1773
Areesha-Irfan <163674228+Areesha-Irfan@users.noreply.github.com>
2025-02-28 22:23:32 +0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,10 @@
+adj1 = input('put any suitable adjective')
+verb = input('put any suitable adj verb')
+adj2 = input('put any suitable adjective')
+bodypart = input('put any suitable body part')
+noun = input('put any suitable noun')
+adj3 = input('put any suitable adjective')
+story = f'In a quiet village, there was an old {adj1} apothecary who was known for creating mysterious potions.One day, a traveler came in seeking a potion that could {verb}. The apothecary warned them that the potion came with a  \
+ {adj2} side effect. Despite the warning, the traveler drank the potion and immediately felt their {bodypart} turn into a {noun}! Now, the traveler must search for a cure while facing the challenges of having a {adj3} new form.'
+
+print(story)  
